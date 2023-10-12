71e0f0f01ff60dd9d443d5ae60485f84c61fc7ec
Sam W <sam.w.2293@gmail.com>
2023-10-12 01:02:10 +0100

    added route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 let persons = [];
 
+app.get("/", (request, response) => {
+  response.send("<h1>Something!</h1>");
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
