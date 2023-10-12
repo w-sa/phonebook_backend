a944f32eb9397999db0e9f099923e041696acaf1
Sam W <sam.w.2293@gmail.com>
2023-10-12 01:23:13 +0100

    added info route

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-let persons = [];
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
 app.get("/", (request, response) => {
-  response.send("<h1>Something else!!</h1>");
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const timestamp = Date.now();
+  const date = new Date(timestamp);
+  const formattedDate = date.toString();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${formattedDate}</p>`
+  );
 });
 
 const PORT = 3001;
